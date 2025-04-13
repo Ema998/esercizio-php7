cceb8dc5de7ffb285e0615f4cc8de1555e73ab42
Ema998 <emanuele.gornati3@gmail.com>
2025-04-13 13:26:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 $message1 = "Inserisci la password: ";
 $message2 = "Riprova.";
-$password = readline(prompt: "$message1\n"); 
-$errors = [];
+$password = readline(prompt: "$message1\n");
+$containsNumber = false;
+$containsLowercase = false;
+$containsSpecial = false;
 
 //check password
-function checkPassword($password){;
+function checkPassword($password){; 
+    $errors = [];
 
     //password almeno 8 caratteri
     if(strlen($password) > 8) {
@@ -17,44 +20,58 @@ function checkPassword($password){;
     //contenere almeno un numero
     for($i = 0; $i < strlen($password); $i++) {
         if(is_numeric($password[$i])) {
-            echo "La password contiene almeno un numero.\n";
-            break;
-        }else{
-            $errors[] = "La password non contiene numeri.\n";
+           $containsNumber = true;
+           break;
         };
     };
-    
+    if ($containsNumber){
+        echo "La password contiene almeno un numero.\n";
+    }else{
+        $errors[] = "La password non contiene numeri.\n";
+    }
+
     //contenere almeno una maiuscola
     for($i = 0; $i < strlen($password); $i++) {
         if(ctype_lower($password[$i])) {
-            echo "La password contiene almeno una lettera minuscola.\n";
+            $containsLowercase = true;
             break;
-        }else{
-            $errors[] = "La password non contiene lettere minuscole.\n";
         };
     };
+    if($containsLowercase){
+        echo "La password contiene almeno una lettera minuscola.\n";
+        }else{
+            $errors[] = "La password non contiene lettere minuscole.\n";
+    }
     
     //contenere almeno un carattere speciale
     for($i = 0; $i < strlen($password); $i++) {
         if(!ctype_alnum($password[$i])) {
-            echo "La password contiene almeno un carattere speciale.\n";
+            $containsSpecial = true;
             break;
-        }else{
-            $errors[] = "La password non contiene caratteri speciali.\n";
-        };
+        }
     };
+    if($containsSpecial){
+        echo "La password contiene almeno un carattere speciale.\n";
+    }else{
+        $errors[] = "La password non contiene caratteri speciali.\n";
+    };
+
     return $errors;
 }
 
 //risultato
+$errors = checkPassword($password);
+
 if(empty($errors)){
     echo "Password corretta.\n";
 }else{
     echo "Password non corretta.\n";
     foreach($errors as $error) {
         echo $error;
-        $password = readline(prompt: "$message2\n");
     };
+    
+    $password = readline(prompt: "$message2\n");
+    $errors = checkPassword($password);
 };
 
 
